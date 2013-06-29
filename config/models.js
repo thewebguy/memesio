e1182bbc76c444fa47ac7fd6798f48092a10e83e
thewebguy <kevinkhandjian@gmail.com>
2013-06-29 13:57:40 -0400

    gitignore, mongo url

--- config/models.js
+++ config/models.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 	, ShortId = require('shortid')
 	, db;
 
-mongoose.connect('mongodb://localhost/memesio');
+mongoose.connect(MONGOHQ_URL || 'mongodb://localhost/memesio');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
